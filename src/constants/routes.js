e155596679623535a1d2d48d9a04f6a6853b39b1
Sonu Sharma <sonu.sharma045@gmail.com>
2021-12-16 21:35:49 +0530

    Add login screen Add firebase login method

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,2 +1,7 @@
 export const LOGIN_ROUTE = 'login';
 export const NO_PAGE_FOUND_ROUTE = '*';
+export const SIGNUP_ROUTE = 'signup';
+export const DASHBOARD = '/';
+export const PROFILE = 'profile';
+export const POST_ROUTE = 'post';
+export const POST_DETAIL_ROUTE = 'post-detail';
--- src/pages/login.page.js
+++ src/pages/login.page.js
@@ -1,9 +1,124 @@
+import { useNavigate } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import FirebaseContext from "../context/firebase";
+import cx from "classnames";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import * as Route from "../constants/routes";
+import { Link } from "react-router-dom";
 
+function Login() {
+  const history = useNavigate();
+  const firebase = useContext(FirebaseContext);
 
-function Login(){
-    return (
-        <div>Hello</div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isValid = password === "" || email === "";
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          history(Route.DASHBOARD);
+        })
+        .catch((error) => {
+          console.log(error.code);
+          switch (error.code) {
+            case "auth/invalid-email":
+              setError("Email or password is invalid");
+              break;
+            case "user-not-found":
+              setError("User not exists"); 
+              break;
+            case "auth/wrong-password":
+              setError("Wrong password");
+              break;
+            
+            default: 
+            console.log(error.code);
+            console.log(error.message);
+            setError("Some thing went wrong. Please try again later");
+              break;
+          }
+        });
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  useEffect(() => {
+    document.title = "Login - instagram";
+  }, []);
+
+  return (
+    <div className="container mx-auto flex max-w-screen-md h-screen">
+      <div className="flex w-3/5  m-auto ">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone" />
+      </div>
+
+      <div className="flex flex-col w-2/5 place-content-center ">
+        <div className="bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justfy-center place-content-center  w-full">
+            <img
+              src="/images/logo.png"
+              alt="instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+          {error && (
+            <p className="text-red-500 mb-4 text-sm  w-full">{error}</p>
+          )}
+
+          <form onSubmit={handleLogin} method="post">
+            <input
+              aria-label="Enter your email address"
+              className="text-sm text-gray-base w-full
+             py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+              type="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              aria-label="Enter your email password"
+              className="text-sm text-gray-base w-full
+            py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              disable={isValid}
+              type="submit"
+              className={cx(
+                "bg-blue-500 text-white w-full py-3 rounded font-bold px-4 rounded mb-2",
+                
+              )}
+            >
+              Log in
+            </button>
+          </form>
+        </div>
+        <div className="flex justfy-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
+          <p className="text-sm">
+            Don't have an account?{" "}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default  Login;
+export default Login;
+
+//text-red-primary
+//text-gray-base
+// border-grey-primary
+// bg-blue-medium
